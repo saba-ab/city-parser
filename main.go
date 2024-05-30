ed5cddf5983d3b3c5c0620bfb25ba973b1eaa5e5
saba-ab <sabaabjandadze@gmail.com>
2024-05-30 20:53:52 +0400

    feat(parser) add parsing logic of different structure htmls, counted parsed streets by cities and total streets, ready for seeding db

--- main.go
+++ main.go
@@ -1,10 +1,57 @@
 package main
 
-import "github.com/saba-ab/city-parser/parser"
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/saba-ab/city-parser/parser"
+)
 
 func main() {
-	err := parser.ParseFile("data/batumi.html")
-	if err != nil {
-		panic(err)
+	directories := []string{"raw_data_ss", "raw_data_myhome"}
+	var allCities []parser.City
+	totalStreetCount := 0
+
+	for _, dir := range directories {
+		cities, err := parser.ParseDirectory(dir)
+		if err != nil {
+			log.Fatalf("Error parsing directory %s: %v", dir, err)
+		}
+		allCities = append(allCities, cities...)
 	}
+
+	// Calculate the total street count
+	for _, city := range allCities {
+		totalStreetCount += city.StreetCount
+	}
+
+	outputData := struct {
+		TotalStreetCount int           `json:"total_street_count"`
+		Cities           []parser.City `json:"cities"`
+	}{
+		TotalStreetCount: totalStreetCount,
+		Cities:           allCities,
+	}
+
+	outputFile := filepath.Join("parsed_data", "cities.json")
+	if err := os.MkdirAll(filepath.Dir(outputFile), 0755); err != nil {
+		log.Fatalf("Failed to create output directory: %v", err)
+	}
+
+	jsonFile, err := os.Create(outputFile)
+	if err != nil {
+		log.Fatalf("Failed to create JSON file: %v", err)
+	}
+	defer jsonFile.Close()
+
+	encoder := json.NewEncoder(jsonFile)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(outputData)
+	if err != nil {
+		log.Fatalf("Failed to write to JSON file: %v", err)
+	}
+
+	log.Printf("Parsed data and created %s\n", outputFile)
 }
